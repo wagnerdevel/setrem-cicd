7679578f8e1b6e4311c2bb89439ae58e43a066a3
Wagner Silveira <wagnerdevel@gmail.com>
2020-04-17 18:48:54 -0300

    Test run 32

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ type Server struct{}
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
    w.WriteHeader(http.StatusOK)
    w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": "hello world 3"}`))
+   w.Write([]byte(`{"message": "hello world"}`))
 }
 func main() {
    s := &Server{}
